08d2aaa18e51f16f98825fe1e7164e0d8e942bb0
yeonghun <126738945+yeunghun@users.noreply.github.com>
2023-05-16 22:53:08 +0900

    Create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st 
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt # pip install matplotlib
+
+st.title("MAP")
+df = pd.DataFrame(np.random.randn(500,2)/[50,50]+[37.76,-122.4],columns=['lat','lon'])
+st.map(df)
+# x = st.slider('Select a value')
+# st.write(x, 'squared is', x*x)
+
+rand = np.random.normal(1, 1, size=20)
+fig, ax = plt.subplots()
+ax.hist(rand, bins=15)
+st.pyplot(fig)
