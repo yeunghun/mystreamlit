7a3b46b23dddac17d9a4c8081f98e12190886974
yeonghun <126738945+yeunghun@users.noreply.github.com>
2023-06-10 01:36:55 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st 
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
 st.title("MAP")
 df = pd.DataFrame(np.random.randn(500,2)/[50,50]+[37.76,-122.4],columns=['lat','lon'])
